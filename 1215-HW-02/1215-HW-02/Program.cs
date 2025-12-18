2037b3da0f0697b5a379e9fa3989125bdc1ef339
Joanne H <jlsjoanne@gmail.com>
2025-12-18 10:18:36 +0800

    feat: connect to github, pull from remote, and create hw-02

---
+++ 1215-HW-02/1215-HW-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1215_HW_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //ch3
+        //3-1
+
+        static void ch3_3_1()
+        {
+            int min = int.MaxValue;
+
+            Console.WriteLine("");
+        }
+    }
+}
