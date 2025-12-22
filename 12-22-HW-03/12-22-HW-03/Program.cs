85ddfc066a1920f9b57a22e4cdb438a3f4786404
Joanne H <jlsjoanne@gmail.com>
2025-12-22 11:24:26 +0800

    feat: add HW-03 project

---
+++ 12-22-HW-03/12-22-HW-03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_22_HW_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        //ch5
+        //5_1
+        static void ch5_5_1()
+        {
+            int[] arr = new int[10];
+
+            Console.WriteLine("5-1.寫一程式，將10個數字讀入A陣列，然後逐一檢查此陣列，如A[i]>5，則令A[i]=A[i]-5，否則A[i]=A[i]+5");
+
+
+            for (int i= 0;i < arr.Length; i++)
+            {
+                Console.Write($"讀入第{i+1}個數字");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                if (arr[i] > 5)
+                {
+                    arr[i] -= 5;
+                }
+                else
+                {
+                    arr[i] += 5;
+                }
+            }
+
+            Console.Write("Array output is: ");
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+
+        }
+    }
+}
