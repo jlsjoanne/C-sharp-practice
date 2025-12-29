5cfdeb883ce484a11c17ece80f17b8409c8bc6ba
Joanne H <jlsjoanne@gmail.com>
2025-12-29 09:15:04 +0800

    feat: add 1229-HW-ALL folder

---
+++ 1229-HW-ALL/1229-HW-ALL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1229_HW_ALL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
